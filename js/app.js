cbe4f5f4e7f485c8c8f42cd728f93d5befca3971
Ronny Perez <ronnypdev@gmail.com>
2020-08-11 16:57:11 -0400

    Cleaning up our code with functions

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 //Scripts
-const tag = document.querySelector('h2');
-tag.textContent = 'A Bronx Base Engineer';
+let pageNumber = 0;
+const outputTag = document.querySelector('h2');
+const nextTag = document.querySelector('.next');
+
+const next = () => {
+  pageNumber = pageNumber + 1;
+  updateSection();
+};
+
+const updateSection = function () {
+  outputTag.innerHTML = pageNumber;
+};
+
+nextTag.addEventListener('click', function () {
+  next();
+});
