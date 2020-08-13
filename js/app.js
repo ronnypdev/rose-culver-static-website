2a441d2e2f08f8f43011e119a3c28ec802dc4201
Ronny Perez <ronnypdev@gmail.com>
2020-08-13 13:10:51 -0400

    Adding in Javascript comments

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
-//Scripts
+// this is the data for holding which page we're on
 let pageNumber = 0;
+
+// Select Relevant Tags
 const outputTag = document.querySelector('h2');
 const nextTag = document.querySelector('.next');
+const previousTag = document.querySelector('.prev');
 
+// make a next function to increase the pageNumber
 const next = () => {
   pageNumber = pageNumber + 1;
 
@@ -13,10 +17,28 @@ const next = () => {
   updateSection();
 };
 
+// Make a previous function decrease the pageNumber
+const previous = () => {
+  pageNumber = pageNumber - 1;
+
+  if (pageNumber < 1) {
+    pageNumber = 10;
+  }
+
+  updateSection();
+};
+
+// This will update the section's content and style
 const updateSection = function () {
   outputTag.innerHTML = pageNumber;
 };
 
+// on click of nextTag, run this
 nextTag.addEventListener('click', function () {
   next();
+});
+
+// on click of previousTag, run this
+previousTag.addEventListener('click', function () {
+  previous();
 });
